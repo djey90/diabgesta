1e3117475dd551b2db78fc1343ec6c29e416dbda
djey90 <msdjeynab@gmail.com>
2020-08-26 23:38:28 +0200

    apropos

--- vues/apropos.php
+++ vues/apropos.php
@@ -13,6 +13,21 @@
             </div>
         </div>
     </section>
+
+    <section class="story">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Mon histoire</h2>
+            </div>
+            <div class="col-md-12">
+                <h4>Les débuts</h4>
+            </div>
+            <div class="col-md-12">
+                <p> Septembre 2016, je suis au téléphone avec une de mes proches amie, quand je lui dis que depuis quelques jours j'ai des douleurs au niveau de la poitrine, des petites nausées, des maux de tête, de petites douleurs au niveau du bas du ventre côté gauche et de perte marron. Elle me dit "je suis sûr que tu es enceinte".</p>
+                <p>Sourire aux lévres, car je savais au fond de moi qu'il y a un petit être qui commencer à prendre forme dans mon ventre. Je fais donc quelques jours plus tard un test de grossesse qui se révele positif.</p>
+            </div>
+        </div>
+    </section>
 </div>
 
 
